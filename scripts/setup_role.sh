2a715c3eaa12ff3cd7bb792abee78d3b77f5c853
Arthur Chi <arthurchi93@gmail.com>
2024-03-20 14:10:48 -0700

    setup_role script

---
+++ scripts/setup_role.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# This script creates a role named SageMakerRole
+# that can be used by SageMaker and has Full access to S3.
+
+ROLE_NAME=SageMakerRole
+
+# Creates a AWS policy for full access to sagemaker
+POLICY=arn:aws:iam::aws:policy/AmazonSageMakerFullAccess
+
+
+cat <<EOF > /tmp/assume-role-policy-document.json
+{
+	"Version": "2012-10-17",
+	"Statement": [{
+		"Effect": "Allow",
+		"Principal": {
+			"Service": "sagemaker.amazonaws.com"
+		},
+		"Action": "sts:AssumeRole"
+	}]
+}
+EOF
+
+# Creates the role
+echo "SAGEMAKER_ROLE=`aws iam create-role --role-name ${ROLE_NAME} --assume-role-policy-document file:///tmp/assume-role-policy-document.json | grep -Eo '"arn:aws:iam.*?[^\\]"'`" >> .env  
+# attaches the S3 full access policy to the role
+aws iam attach-role-policy --policy-arn ${POLICY}  --role-name ${ROLE_NAME}
