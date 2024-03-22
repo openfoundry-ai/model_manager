a84e4ca2b5c77c492942620f4655d7605ed8c8ad
Arthur Chi <arthurchi93@gmail.com>
2024-03-22 10:54:58 -0700

    fix: installer script for AWS CLI for linux

--- setup.sh
+++ setup.sh
@@ -4,8 +4,22 @@ make
 source venv/bin/activate
 if ! command -v aws &> /dev/null
 then
-    curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
-    sudo installer -pkg ./AWSCLIV2.pkg -target /
+    OS="$(uname -s)"
+    case "${OS}" in
+        Linux*)     
+            curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+            unzip awscliv2.zip
+            sudo ./aws/install
+            ;;
+        Darwin*)    
+            curl "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
+            sudo installer -pkg AWSCLIV2.pkg -target /
+            ;;
+        *)          
+            echo "Unsupported OS: ${OS}. See https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html"
+            exit 1
+            ;;
+    esac
 fi
 aws configure set region us-east-1 && aws configure
 touch .env
