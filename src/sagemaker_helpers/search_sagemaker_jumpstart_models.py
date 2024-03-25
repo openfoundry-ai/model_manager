d11fe4e657030f1949b99a3c7dfb46a60a48f45a
Arthur Chi <arthurchi93@gmail.com>
2024-03-25 14:44:36 -0700

    uses fuzzy match to better filter sagemaker models

--- src/sagemaker_helpers/search_sagemaker_jumpstart_models.py
+++ src/sagemaker_helpers/search_sagemaker_jumpstart_models.py
@@ -25,21 +25,12 @@ def search_sagemaker_jumpstart_model():
                       message="Which framework would you like to use?",
                       choices=[framework.value for framework in Frameworks]
                       ),
-        inquirer.Text(
-            'filter', message="Enter some text to search for a SageMaker model (e.g. ‘bert’)")
     ]
     answers = inquirer.prompt(questions)
     if answers is None:
         return
-    model_filter = answers["filter"] or None
     filter_value = "framework == {}".format(answers["framework"])
 
     models = list_jumpstart_models(filter=filter_value,
                                    region=session.region_name, sagemaker_session=sagemaker_session)
-
-    if model_filter is not None:
-        models = list(filter(lambda x: model_filter in x, models))
-
-    if len(models) == 0:
-        print_error("No models found. Please try another")
     return models
